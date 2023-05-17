c5324edd23a942af7f7f33cf40a045f3c68741cc
碳化硅 <99667304+stevew1007@users.noreply.github.com>
2023-05-17 23:41:42 +0800

    feat(posts): loading states for post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,8 +9,9 @@ import type { RouterOutputs } from "~/utils/api";
 
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
-import { LoadingPage } from "~/components/loading";
+import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
+import { toast } from "react-hot-toast";
 
 dayjs.extend(relativeTime);
 
@@ -23,14 +24,24 @@ const CreatePostWizard = () => {
   const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
     onSuccess: () => {
       setInput("");
+      toast.success("Post created!");
       void ctx.posts.getAll.invalidate();
+    },
+    onError: (e) => {
+      console.log("e::: ", e);
+      const errorMessage = e.data?.zodError?.fieldErrors.content;
+      if (errorMessage && errorMessage[0]) {
+        toast.error(errorMessage[0]);
+      } else {
+        toast.error("Something went wrong!");
+      }
     },
   });
   // mutate({ content: "Hello World!" });
 
   if (!user) return null;
   return (
-    <div className="flex gap-3">
+    <div className="flex w-full gap-3">
       <UserButton />
       <input
         placeholder="Type something..."
@@ -39,8 +50,25 @@ const CreatePostWizard = () => {
         type="text"
         onChange={(e) => setInput(e.target.value)}
         disabled={isPosting}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            e.preventDefault();
+            if (input !== "") {
+              mutate({ content: input });
+            }
+          }
+        }}
       />
-      <button onClick={() => mutate({ content: input })}>Post</button>
+      {input !== "" && (
+        <button onClick={() => mutate({ content: input })} disabled={isPosting}>
+          Post
+        </button>
+      )}
+      {isPosting && (
+        <div className="flex items-center justify-center">
+          <LoadingSpinner size={20} />
+        </div>
+      )}
     </div>
   );
 };
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -64,7 +64,7 @@ export const postsRouter = createTRPCRouter({
     });
   }),
   create: privateProcedure
-    .input(z.object({ content: z.string().min(1).max(280) }))
+    .input(z.object({ content: z.string().min(3).max(280) }))
     .mutation(async ({ ctx, input }) => {
       const authorId = ctx.userId;
       const { success } = await ratelimit.limit(authorId);
